bab179d2a14f8c5e48da95b39988b47e7745a576
Eccenux <eccenux+git@gmail.com>
2021-01-24 14:39:15 +0100

    column ends

--- svgToColumns.php
+++ svgToColumns.php
@@ -9,6 +9,7 @@ require_once './config.php';
 
 $basePath = BASE_PATH;
 $baseUrl = BASE_URL;
+$minColumnWidth = 10;
 $svgPath = "{$basePath}columns.edit.svg";
 $origPath = "{$basePath}columns.json";
 $outputPath = "{$basePath}columns.edit.json";
@@ -17,19 +18,35 @@ $outputPath = "{$basePath}columns.edit.json";
 $json = file_get_contents("{$basePath}columns.json");
 $columnsMeta = MetaColumns::fromJson($json);
 
-// read column ends
+// read SVG
 $svg = new SvgAnalyze($svgPath);
 $lines = $svg->getNodesByClass('line');
 // echo "\n".count($lines['top'])."\n";
 // echo "\n".count($lines['column'])."\n";
+
+// read top
 if (!empty($lines['top'])) {
 	$node = $lines['top'][0];
-	$top = SvgAnalyze::getAttribute($node, 'y1');
+	$top = intval(SvgAnalyze::getAttribute($node, 'y1', 0));
 	$columnsMeta->top = $top;
 	echo "\n[INFO] Top found: $top";
 }
 
-echo "\n". $columnsMeta->toJson();
+// read column ends
+if (!empty($lines['column'])) {
+	echo "\n[INFO] Ends found, count: ". count($lines['column']);
+	$ends = array();
+	foreach ($lines['column'] as $node) {
+		$end = intval(SvgAnalyze::getAttribute($node, 'x1', 0));
+		if ($end >= $minColumnWidth) {
+			$ends[] = $end;
+		}
+	}
+	$columnsMeta->setEnds($ends);
+}
+
+//echo "\n". $columnsMeta->toJson();
+file_put_contents($outputPath, $columnsMeta->toJson());
 
 // info
 echo "\n{$baseUrl}{$outputPath}";
